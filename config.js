f25d2d51c4432219dfac42ae5edf1425a313f966
CurtisScholey <97024501+CurtisScholey@users.noreply.github.com>
2024-07-10 14:46:54 +0000

    adding comment as to .env

--- config.js
+++ config.js
@@ -1,17 +1,12 @@
-/*
+/* --Code to use dotenv upon upscaling of the project--
 import dotenv from 'dotenv'; //Imports env
-
 dotenv.config(); 
-
 const apiKey = process.env.API_KEY; //Summons api key
-
 console.log(apiKey); //Logging the API key for verification purposes
-
 export { apiKey }; //Exports apikey
 */
 
 const apiKey = "132e8ef7c1d49498628ffb91";
-
 console.log(apiKey);
 
 export { apiKey };
