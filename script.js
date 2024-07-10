42e3fb585947f1a0569c1dc463a1b561e1110022
CurtisScholey <97024501+CurtisScholey@users.noreply.github.com>
2024-07-10 14:45:29 +0100

    page not updating, recommiting

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const currencyTwo = document.getElementById('currencyTwo'); // Second currency dropdown
     const conversionRateDisplay = document.getElementById('conversionRate'); // Display for conversion rate
     //const apiKey = '132e8ef7c1d49498628ffb91'; 
+    
     // Fetch exchange rates and populate dropdowns
     async function fetchExchangeRates(baseCurrency = 'USD') {
         // Fetch exchange rates from the API using the base currency
